cde2e2a748889d4e54eed508e7eb35aa9a724af7
opope12 <opope@me.com>
2013-05-02 22:11:10 -0400

    #3
    Header.php

---
+++ users/header.php
@@ -0,0 +1,12 @@
+<div class="container">
+	<div class="navbar">
+		<div class="navbar-inner">
+			<a class="brand" href="#">Title</a>
+			<ul class="nav">
+				<li class="active"><a href="#">Home</a></li>
+				<li><a href="#">Link</a></li>
+				<li><a href="#">Link</a></li>
+			</ul>
+		</div>
+	</div>
+</div>
